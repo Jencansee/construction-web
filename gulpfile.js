fb8f9528efcc76b15a32fb784151cf59d37d3d3b
Dmitry <jncxnsee@tuta.io>
2020-04-17 21:23:35 +0500

    First commit, been working for two days on the project, there was a repository earlier for this project, but I somehow put two personal files in the directory, so I deleted it and started anew. More than 60% of the page complete

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src('./src/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist/styles/'))
+        .pipe(browserSync.stream());
+})
+
+// CODE BELOW ASKS GULP TO WATCH HTML, SCSS, JS on Change and after edit browserSync reloads tabs (also the code is initializing the server in the dir by brwSync)
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: './'
+    })
+
+    gulp.watch('./src/sass/*.scss', gulp.series('sass'))
+    gulp.watch('./*.html').on('change', browserSync.reload)
+    gulp.watch('./dist/scripts/*.js').on('change', browserSync.reload)
+})
